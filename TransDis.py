e27ee34e730012c0499b7f15f696e935f2a788a1
TransHeartStudios <95623073+TransHeartStudios@users.noreply.github.com>
2022-07-15 06:40:41 -0500

    deleted
    deleted

--- TransDis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv ('Dis.csv')
-df = df[df.CountryCode == 'Average']
-df = df[df.answer == "Yes"]
-df = df[df.subset == "Transgender"]
-df.reset_index(drop=True, inplace=True)
-for i in range(len(df)):
-    print(df['question_label'][i]+'\n'+ df['percentage'][i])
--- firstPython.py
+++
@@ -1,5 +0,0 @@
-# Display The Output
-print('New Python File')
-# changes made on computer
-val = 'GitHub Desktop'
-print(f'These are changes made from {val}')
